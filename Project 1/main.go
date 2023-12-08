7e870e92f4c8faca1970bb3e8cd6e0d00a9caa26
Gromovoi Stanislav <gromila12@rambler.ru>
2023-12-08 12:32:03 +0600

    Project 1

--- Project 1/main.go
+++ Project 1/main.go
@@ -4,14 +4,17 @@ import (
     "net/http"
     
     "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func setupRouter() *echo.Echo {
 	e := echo.New()
     
+
 	e.GET("/", func(c echo.Context) error {
         return c.String(http.StatusOK, "Hello, World!")
     })
+
 
 	e.GET("/ping", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{
@@ -19,21 +22,72 @@ func setupRouter() *echo.Echo {
 		})
 	})
 
-	e.POST("/users/:name", func(c echo.Context) error {
-		name := c.Param("name")
+
+	e.GET("/show", func(c echo.Context) error {
+		team := c.QueryParam("team")
+		member := c.QueryParam("member")
+
+		return c.JSON(http.StatusOK, map[string]string{
+			"team": team,
+			"member": member,
+		})
+    })
+
+
+	e.GET("/users/:id", func(c echo.Context) error {
+		id := c.Param("id")
 
 		responseData := map[string]interface{}{
-			"id":   1,
-			"name": name,
+			"id":   id,
+			"name": "John",
 		}
 		
 		return c.JSON(http.StatusOK, responseData)
 	})
+
+
+	e.POST("/users", func(c echo.Context) error {
+		type User struct {
+			Id  string `json:"id" xml:"id"`
+			Name  string `json:"name" xml:"name" form:"name" query:"name"`
+			Email string `json:"email" xml:"email" form:"email" query:"email"`
+		}
+
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		u.Id = "1"
+	
+		return c.JSON(http.StatusCreated, u)
+		// or
+		//return c.XML(http.StatusCreated, u)
+	})
+
+
+	// Group level middleware
+	g := e.Group("/admin")
+	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+	if username == "joe" && password == "secret" {
+		return true, nil
+	}
+		return false, nil
+	}))
+
+	g.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Hello, Admin!")
+    })
+
 
 	return e
 }
 
 func main() {
 	e := setupRouter()
+
+	// Middleware
+	// e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
--- Project 1/main_test.go
+++ Project 1/main_test.go
@@ -4,7 +4,9 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
+	"strings"
+	"encoding/json"
+	
 	"github.com/stretchr/testify/assert"
 )
 
@@ -33,14 +35,75 @@ func TestPingRoute(t *testing.T) {
 	assert.JSONEq(t, `{"message":"pong"}`, w.Body.String())
 }
 
-func TestUserRoute(t *testing.T) {
+
+func TestShowRoute(t *testing.T) {
 	router := setupRouter()
 	
 	w := httptest.NewRecorder()	
 
-	req, _ := http.NewRequest("POST", "/users/Tom", nil)
+	req, _ := http.NewRequest("GET", "/show?team=x-men&member=wolverine", nil)
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
-	assert.JSONEq(t, `{"id":1, "name":"Tom"}`, w.Body.String())
+	assert.JSONEq(t, `{"team":"x-men","member":"wolverine"}`, w.Body.String())
+}
+
+
+func TestGetUserRoute(t *testing.T) {
+	router := setupRouter()
+	
+	w := httptest.NewRecorder()	
+
+	req, _ := http.NewRequest("GET", "/users/1", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.JSONEq(t, `{"id":"1", "name":"John"}`, w.Body.String())
+}
+
+func TestPostUserRoute(t *testing.T) {
+	router := setupRouter()
+	
+	w := httptest.NewRecorder()	
+
+	// Подготавливаем JSON-тело запроса
+	// jsonBody := `{"name":"Tom","email":"tom@mail.com"}`
+	// body := strings.NewReader(jsonBody)
+
+	// Подготавливаем map с данными пользователя
+	userData := map[string]interface{}{
+		"name":  "Tom",
+		"email": "tom@mail.com",
+	}
+
+	// Кодируем map в JSON
+	jsonBody, err := json.Marshal(userData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Создаем тело запроса из JSON
+	body := strings.NewReader(string(jsonBody))
+
+
+	req, _ := http.NewRequest("POST", "/users", body)
+	req.Header.Set("Content-Type", "application/json")
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 201, w.Code)
+	assert.JSONEq(t, `{"id":"1", "name":"Tom","email":"tom@mail.com"}`, w.Body.String())
+}
+
+func TestAdminRoute(t *testing.T) {
+	router := setupRouter()
+	
+	w := httptest.NewRecorder()	
+
+	req, _ := http.NewRequest("GET", "/admin/", nil)
+	req.Header.Add("Authorization", "Basic am9lOnNlY3JldA==")
+
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "Hello, Admin!", w.Body.String())
 }
